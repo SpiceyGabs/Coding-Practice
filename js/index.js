753acf4ed42d870442d0ebd451beccad85ed558c
SpiceyGabs <sgsquared.2@gmail.com>
2025-05-22 18:09:12 +0200

    If this doesnt work im moving onto the toggle menu

--- js/index.js
+++ js/index.js
@@ -1,24 +1,29 @@
- document.addEventListener('DOMContentLoaded', function () {
-    const nextBtn = document.getElementById('nextBtn');
-    const introBanner = document.getElementById('introBanner');
+// 3. nextBtn.addEventListener("click", ()=> 
+//     {}                                           //DOM: listening for interaction
+//  document.getElementById("mainHeader").scrollIntoView({ behavior: "smooth" });
+//  document.addEventListener('DOMContentLoaded', function () {
+//     const nextBtn = document.getElementById('nextBtn');
+//     const introBanner = document.getElementById('introBanner');
 
-    nextBtn.addEventListener('click', function () {
-      introBanner.style.display = 'none';
-      document.body.style.overflow = 'auto';
-    });
-  });
+//     nextBtn.addEventListener('click', function () {
+//       introBanner.style.display = 'none';
+//       document.body.style.overflow = 'auto';
+//     });
+//   });
 
-  //const nextBtn = document.getElementById("nextBtn");  // selecting a button from the DOM
-//const banner = document.getElementById("introBanner"); // selecting a div
+const nextBtn = document.getElementById("nextBtn");  // selecting a button from the DOM
+const banner = document.getElementById("introBanner"); // selecting a div
 
-  //Animation
-  //nextBtn.addEventListener("click", ()=> { //DOM: listening for interaction
-   // {}
-    //banner.style.transform = "translateY(-100%)" ; // slide up, changes the style
-    //banner.style.position = "absolute"; // this prevents the button from staying fixed
-    //banner.style.height = "100px"; // shrink effect
- // });
-// document.getElementById("mainHeader").scrollIntoView({ behavior: "smooth" });
+  Animation
+  nextBtn.addEventListener("click", ()=> { //DOM: listening for interaction
+   {}
+    banner.style.transform = "translateY(-100%)" ; // slide up, changes the style
+    banner.style.position = "absolute"; // this prevents the button from staying fixed
+    banner.style.height = "100px"; // shrink effect
+ });
+
+
+//2. document.getElementById("mainHeader").scrollIntoView({ behavior: "smooth" });
 // document.addEventListener("DOMContentLoaded", function () {
 //   const nextBtn = document.getElementById("nextBtn");
 //   const banner = document.getElementById("introBanner");
