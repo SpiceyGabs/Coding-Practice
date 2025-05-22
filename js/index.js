934d6cf79c893d8f754e20f3a836066426f6d5e3
SpiceyGabs <sgsquared.2@gmail.com>
2025-05-22 15:29:14 +0200

    fixing js animations and my index styling oh wow

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-<script>
 
-  const nextBtn = document.getElementById("nextBtn");
-  const banner = document.getElementById("introBanner");
+  const nextBtn = document.getElementById("nextBtn");  // selecting a button from the DOM
+  const banner = document.getElementById("introBanner"); // selecting a div
 
-  nextBtn.addEventListener("click", ()= {
-    banner.style.transform = "translateY(-100%)"; // slide up
+  //Animation
+  nextBtn.addEventListener("click", ()=> { //DOM: listening for interaction
+    {}
+    banner.style.transform = "translateY(-100%)" ; // slide up, changes the style
     banner.style.position = "absolute"; // this prevents the button from staying fixed
     //banner.style.height = "100px"; // shrink effect
   });
-</script>
