93904d1d4f9d5827f05af9577ec9ae72050306f2
SpiceyGabs <sgsquared.2@gmail.com>
2025-05-23 02:20:02 +0200

    Resetting ALL recent changes, crying in the club

--- js/index.js
+++ js/index.js
@@ -1,11 +1,35 @@
-<script>
+// 3. nextBtn.addEventListener("click", ()=> 
+//     {}                                           //DOM: listening for interaction
+//  document.getElementById("mainHeader").scrollIntoView({ behavior: "smooth" });
+//  document.addEventListener('DOMContentLoaded', function () {
+//     const nextBtn = document.getElementById('nextBtn');
+//     const introBanner = document.getElementById('introBanner');
 
-  const nextBtn = document.getElementById("nextBtn");
-  const banner = document.getElementById("introBanner");
+//     nextBtn.addEventListener('click', function () {
+//       introBanner.style.display = 'none';
+//       document.body.style.overflow = 'auto';
+//     });
+//   });
 
-  nextBtn.addEventListener("click", ()= {
-    banner.style.transform = "translateY(-100%)"; // slide up
+const nextBtn = document.getElementById("nextBtn");  // selecting a button from the DOM
+const banner = document.getElementById("introBanner"); // selecting a div
+
+  //Animation
+  nextBtn.addEventListener("click", ()=> { //DOM: listening for interaction
+   {}
+    banner.style.transform = "translateY(-100%)" ; // slide up, changes the style
     banner.style.position = "absolute"; // this prevents the button from staying fixed
-    //banner.style.height = "100px"; // shrink effect
-  });
-</script>
+    banner.style.height = "100px"; // shrink effect
+ });
+
+
+//2. document.getElementById("mainHeader").scrollIntoView({ behavior: "smooth" });
+// document.addEventListener("DOMContentLoaded", function () {
+//   const nextBtn = document.getElementById("nextBtn");
+//   const banner = document.getElementById("introBanner");
+
+//   nextBtn.addEventListener("click", () => {
+//     banner.classList.add("hide"); // triggers the CSS animation
+//     document.body.style.overflow = "auto"; // re-enable scroll
+//   });
+// });
